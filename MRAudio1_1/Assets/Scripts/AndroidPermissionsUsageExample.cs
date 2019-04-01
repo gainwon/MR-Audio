321fa53e78d64768d4d9a568d05ae8e40563d904
Gainwon msi <contact@gainwon.com>
2019-04-01 16:00:11 +0300

    MRAudio1_1
    Created the project + added the test scene.

---
+++ MRAudio1_1/Assets/Scripts/AndroidPermissionsUsageExample.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AndroidPermissionsUsageExample : MonoBehaviour
+{
+
+    public Text text;
+     
+    public void MicInput()
+    {
+        text.text = "Start Rec..";
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = Microphone.Start(null, true, 1, 22050);
+        audio.loop = true;
+        while (!(Microphone.GetPosition(null) > 0)) { }
+        text.text = "start playing... position is " + Microphone.GetPosition(null);
+        audio.Play();
+    }
+
+    public void BeginListener(int index)
+    {
+        int min = 0;
+        int max = 0;
+
+        AudioSource _AudioSource = GetComponent<AudioSource>();
+
+        Microphone.GetDeviceCaps(Microphone.devices[index], out min, out max);
+
+
+        _AudioSource.clip = Microphone.Start(Microphone.devices[index], true, 2, max);
+
+        while (!(Microphone.GetPosition(Microphone.devices[index]) > 1))
+        {
+            // Wait until the recording has started
+        }
+
+        _AudioSource.loop = true;
+        _AudioSource.Play();
+    }
+
+    private const string RECORDAUDIO_PERMISSION = "android.permission.RECORD_AUDIO";
+
+
+
+    public void OnGrantButtonPress()
+    {
+        text.text = "On Grant..";
+        AndroidPermissionsManager.RequestPermission(new[] { RECORDAUDIO_PERMISSION }, new AndroidPermissionCallback(
+             grantedPermission =>
+             {
+                 text.text = "Accepted"; 
+             },
+            deniedPermission =>
+            {
+                // The permission was denied
+            },
+            deniedPermissionAndDontAskAgain =>
+            {
+                // The permission was denied, and the user has selected "Don't ask again"
+                // Show in-game pop-up message stating that the user can change permissions in Android Application Settings
+                // if he changes his mind (also required by Google Featuring program)
+            }));  
+    }
+
+
+    private bool CheckPermissions()
+    { 
+        return AndroidPermissionsManager.IsPermissionGranted(RECORDAUDIO_PERMISSION);
+    }
+}
